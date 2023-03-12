e8739c4f8c8b55f1607aea057105eb0ffcde4e32
Juan Manuel Cugat <juanma.cugat@gmail.com>
2023-03-12 21:00:26 +0100

    use give votes instaed of mint

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {ethers} from 'ethers';
 import * as tokenJson from './assets/MyToken.json';
+import * as ballotJson from './assets/Ballot.json';
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -25,10 +26,10 @@ export class AppService {
     console.log(`Request tokens: ${amount}`);
     const provider = new ethers.providers.InfuraProvider("goerli", process.env.INFURA_API_KEY);
     const wallet = new ethers.Wallet(privateKey, provider);
-    const contract = new ethers.Contract(TOKEN_ADDRESS, tokenJson.abi, wallet);
+    const contract = new ethers.Contract(BALLOT_ADDRESS, ballotJson.abi, wallet);
     
     const parsedAmount = ethers.utils.parseEther(amount.toString());
-    const mintTx = await contract.mint(address,parsedAmount);
+    const mintTx = await contract.giveVotes(address,parsedAmount);
     const receipt = await mintTx.wait();
     console.log("Receipt: " , receipt);
     console.log("Tx hash:", receipt.transactionHash);
